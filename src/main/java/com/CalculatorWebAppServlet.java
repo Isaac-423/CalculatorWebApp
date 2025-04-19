9857da928def949e8bec268cb99ef6586342d75d
Isaac-423 <your.isuruudanapinthu@gmail.com>
2025-04-19 10:03:31 +0530

    init commit

--- src/main/java/com/CalculatorWebAppServlet.java
+++ src/main/java/com/CalculatorWebAppServlet.java
@@ -1,5 +1,22 @@
 package com;
 
-public class CalculatorWebAppServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/calc")
+public class CalculatorWebAppServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int a = Integer.parseInt(request.getParameter("a"));
+        int b = Integer.parseInt(request.getParameter("b"));
+
+        int sum = (a + b);
+
+        response.getWriter().print("Sum of " + a + "and" + b + "is = " + sum);
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,21 @@
 <html>
+<head>
+    <title>My Calculator</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<div align="center">
+    <h2>Web Calculator</h2>
+    <form action="calc" method = "post">
+        <p>
+            Input number A <input type = "number" name = "a" required/>
+        </p>
+        <p>
+            Input number B <input type = "number" name = "b" required/>
+        </p>
+        <p>
+            <input type = "submit" value="RUN" />
+        </p>
+    </form>
+</div>
 </body>
 </html>
